613bea11a9919a346519d8d8504bc074933632b0
kailash gayari <kailash.g@amal.com>
2025-08-15 15:17:10 +0530

    changes to run build

--- src/concept-list/ConceptsList.tsx
+++ src/concept-list/ConceptsList.tsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Card from '../components/Card';
+import Card from '../components/ReusableCard';
+
 
 
 const concepts = [
