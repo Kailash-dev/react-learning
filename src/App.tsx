6e9df9486677af6d4ec3828cdcfbe320d4528b35
kailash gayari <kailash.g@amal.com>
2025-08-15 15:33:07 +0530

    introducing hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+
 import "./App.css";
 import AppRoutes from "./routes";
 
